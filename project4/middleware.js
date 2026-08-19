00ef18e2d5aad36bad6e0d6bbe614f3977b8d6c0
Sarthak <sarthakhingankar@gmail.com>
2026-08-19 15:51:54 +0530

    project 4

---
+++ project4/middleware.js
@@ -0,0 +1,26 @@
+function inputCleaner(req, res, next) {
+    if (req.body.username) {
+        req.body.username = req.body.username.toLowerCase();
+    }
+
+    if (req.body.comment) {
+        req.body.comment = req.body.comment.replace(/<[^>]*>/g, "");
+    }
+
+    next();
+}
+
+function inputValidator(req, res, next) {
+    if (req.body.username && req.body.username.length >= 3) {
+        next();
+    } else {
+        res.redirect(
+            "/form?error=Username must be at least 3 characters."
+        );
+    }
+}
+
+module.exports = {
+    inputCleaner,
+    inputValidator
+};
